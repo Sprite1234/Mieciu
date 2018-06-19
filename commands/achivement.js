2632121b0f0f172a2fc305532fd9766654eef6b0
Sprite1234 <37411789+Sprite1234@users.noreply.github.com>
2018-06-19 22:18:46 +0200

    make it better

--- commands/achivement.js
+++ commands/achivement.js
@@ -7,6 +7,7 @@ module.exports.run = async(client,message,args){
       files: [`https://www.minecraftskinstealer.com/achievement/a.php?i=2&h=Achievement+Get%21&t=${args.join}%28%22+%22%29`]
     }
   )
+}
 module.exports.help ={
   name: "achivement"
 }
