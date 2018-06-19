96d42d81240fbea49180e258e8a09888695e51c3
Sprite1234 <37411789+Sprite1234@users.noreply.github.com>
2018-06-19 22:59:36 +0200

    make it better

--- commands/achivement.js
+++ commands/achivement.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const fs = require('fs')
 const Jimp = require('jimp')
 module.exports.run = async (client, message, args) => {
-message.channel.send(`https://www.minecraftskinstealer.com/achievement/a.php?i=2&h=Achievement+Get%21&t=${args.join}%28%22+%22%29`)
+message.channel.send(`https://www.minecraftskinstealer.com/achievement/a.php?i=2&h=Achievement+Get%21&t=${args.join(" ")}`)
 }
 module.exports.help ={
   name: "achivement"
