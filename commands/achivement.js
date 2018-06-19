da31759f79368793efde83713263d67983ebe2ac
Sprite1234 <37411789+Sprite1234@users.noreply.github.com>
2018-06-19 21:05:51 +0200

    make it better

---
+++ commands/achivement.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js')
+
+module.exports.run = async(client,message,args){
+  var embed = new Discord.RichEmbed()
+  message.channel.send(
+    {
+      files: [`https://www.minecraftskinstealer.com/achievement/a.php?i=2&h=Achievement+Get%21&t=${args.join}%28%22+%22%29`]
+    }
+  )
+module.exports.help ={
+  name: "achivement"
+}
