0c3c0159f937135298353c84f53012ebfb86498c
Sprite1234 <37411789+Sprite1234@users.noreply.github.com>
2018-06-19 22:28:33 +0200

    make it better

--- commands/achivement.js
+++ commands/achivement.js
@@ -1,13 +1,19 @@
 const Discord = require('discord.js')
-
+const fs = require('fs')
+const Jimp = require('jimp')
 module.exports.run = async (client, message, args) => {
-  var embed = new Discord.RichEmbed()
-  message.channel.send(
-    {
-      files: [`https://www.minecraftskinstealer.com/achievement/a.php?i=2&h=Achievement+Get%21&t=${args.join}%28%22+%22%29`]
-    }
-  )
-}
+  Jimp.read(`https://www.minecraftskinstealer.com/achievement/a.php?i=2&h=Achievement+Get%21&t=${args.join}%28%22+%22%29`).then(async function (image) {
+    // do stuff with the image
+    await image.write('./' + args.join(" ") + ".png")
+  }).catch(function (err) {
+  });
+  fs.readFile('./' + args.join(" ") + ".png", function (err, data) {
+      message.channel.send({
+        files: [data]
+      })
+    })
+  }
 module.exports.help ={
   name: "achivement"
 }
+//`https://www.minecraftskinstealer.com/achievement/a.php?i=2&h=Achievement+Get%21&t=${args.join}%28%22+%22%29`
