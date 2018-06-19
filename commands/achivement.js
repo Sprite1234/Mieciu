967a7fee4cad0aaa11f8e11bbdb8dfb50d25bd8d
Sprite1234 <37411789+Sprite1234@users.noreply.github.com>
2018-06-19 23:13:05 +0200

    make it better

--- commands/achivement.js
+++ commands/achivement.js
@@ -1,8 +1,13 @@
 const Discord = require('discord.js')
 const fs = require('fs')
 const Jimp = require('jimp')
+const config = require(`../config.json`)
 module.exports.run = async (client, message, args) => {
-message.channel.send(`https://www.minecraftskinstealer.com/achievement/a.php?i=2&h=Achievement+Get%21&t=${args.join("+")}`)
+  let embed = new Discord.RichEmbed()
+  .setTitle("Achivement")
+  .setColor(config.embed_color)
+  .setImage(`https://www.minecraftskinstealer.com/achievement/a.php?i=2&h=Achievement+Get%21&t=${args.join("+")}`)
+message.channel.send({embed: embed})
 }
 module.exports.help ={
   name: "achivement"
